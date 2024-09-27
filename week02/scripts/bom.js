93ba064ca3507621e0fc7c12d55205ecb34c00e1
Lucas <neves.lucas.r@gmail.com>
2024-09-27 09:48:59 -0300

    Handling DOM exercise done

---
+++ week02/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+button.addEventListener('click', function() {
+  if(input.value.trim() !== "") {
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+    input.focus()
+  }
+  else {
+    window.alert("Entry cannot be blank.")
+    input.focus()
+  }
+});
+
+deleteButton.addEventListener('click', function() {
+  list.removeChild(li);
+  input.value = '';
+  input.focus();
+});
